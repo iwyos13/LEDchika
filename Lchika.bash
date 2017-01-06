35c49a36a3f72c35db7a0fb3ae8d06e5178b87da
Yoshiaki Iwatani <yoshiwakitani@gmail.com>
2017-01-06 16:13:13 +0900

    Create Lchika.bash

---
+++ Lchika.bash
@@ -0,0 +1,14 @@
+! /bin/bash
+
+for ((i = 0; i < 30; i++)); do
+    echo 1 > /dev/myled0
+    sleep 0.5
+    echo 0 > /dev/myled0
+    sleep 0.2
+    echo 1 > /dev/myled0
+    sleep 0.1
+    echo 0 > /dev/myled0
+    sleep 0.1
+done
+
+//bashコマンドで実行
